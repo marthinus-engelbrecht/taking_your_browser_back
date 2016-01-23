048ee85c1f8e9ce4b94ffd1e892a76097c27eae0
Marthinus Engelbrecht <mj.engelbrecht@gmail.com>
2016-01-23 13:08:47 +0200

    Updated the indentation to be consitent

--- app/components/router/router.js
+++ app/components/router/router.js
@@ -1,32 +1,31 @@
 class Router {
-  constructor(){
-    this._states = [];
-  }
+    constructor() {
+        this._states = [];
+    }
 
-  registerState(state) {
-    this._states.push(state);
-  }
+    registerState(state) {
+        this._states.push(state);
+    }
 
-  getStates(){
-    return this._states;
-  }
+    getStates() {
+        return this._states;
+    }
 
-  goto(path){
-    this._currentState = this._states.find((state) =>{
-        let found = state.path === path;
-        if(found){
-          //Injected
-          this.history.pushState(state.title, null, path);
-          this.document.title = state.title;
-        }
-        return found;
-    });
-  }
+    goto(path) {
+        this._currentState = this._states.find((state) => {
+            let found = state.path === path;
+            if (found) {
+                //Injected
+                this.history.pushState(state.title, null, path);
+                this.document.title = state.title;
+            }
+            return found;
+        });
+    }
 
-  getCurrentState(){
-    return this._currentState;
-  }
+    getCurrentState() {
+        return this._currentState;
+    }
 }
-
 
 export default Router;
